8e9e3a151f3d1c713609055440b04f4a012b3f99
Chris Morgan <chris@itsthemorgans.com>
2014-12-27 16:26:44 -0500

    added favicon and fixed website title

--- index.js
+++ index.js
@@ -25,19 +25,19 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'app_server/views'));
 app.set('view engine', 'html');
 
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(bodyParser.json());
-app.use(express.cookieParser('letsgetitstarted'));
-app.use(express.session());
+//app.use(express.cookieParser('letsgetitstarted'));
+//app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //for dev
-swig.setDefaults({cache: false});
+//swig.setDefaults({cache: false});
 
 // development only
 if ('development' == app.get('env')) {
